3ed18f447bfc1edcafe63c111494b470fc909e52
Ariane valeria <153088012+arianevaleira@users.noreply.github.com>
2024-09-14 09:30:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 #Aplicativo principal
+from flask  import Flask, render_template, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('index.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
